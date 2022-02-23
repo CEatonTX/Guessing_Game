6456eb36ee0f515d95a8cade55caa70f4952ba7e
Chris Eaton <chriseatontx@gmail.com>
2022-02-23 07:59:52 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+
+use std::io;  // library allows input
+use rand::Rng; // random number crate
+use std::cmp::Ordering; // compare from the standard library
+
+fn main() {
+
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    // println!("The secret number is: {}", secret_number);
+
+    loop {
+
+        println!("Please input your guess.");
+
+        let mut guess = String::new();  // mutable variable, by default immutable
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
+    }
+
+}
+
+
